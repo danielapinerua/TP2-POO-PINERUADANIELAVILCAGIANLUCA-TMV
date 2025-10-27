2c2e922f75e1ee9f3d58c1037a8c9f50e9084277
danielapinerua <daniela.pinerua@davinci.edu.ar>
2025-10-27 20:12:52 -0300

    Update TipoUsuario.java

--- src/logicLayer/TipoUsuario.java
+++ src/logicLayer/TipoUsuario.java
@@ -1,5 +1,42 @@
 package logicLayer;
 
+import javax.swing.JOptionPane;
+
 public enum TipoUsuario {
+	CLIENTE("Depositar dinero/Retirar dinero/Ver saldo/Salir"),
+    EMPLEADO("Ver cuentas/Cargar dinero al cajero/Salir");
+
+    private final String opciones;
+
+    private TipoUsuario(String opciones) {
+        this.opciones = opciones;
+    }
+
+    public void mostrarMenu() {
+        String[] opcionesEnum = this.opciones.split("/");
+        String opcion = (String) JOptionPane.showInputDialog(null,
+                "Seleccione una opción:",
+                "Menú de " + this.name(),
+                JOptionPane.DEFAULT_OPTION,
+                null,
+                opcionesEnum,
+                opcionesEnum[0]);
+        JOptionPane.showMessageDialog(null, "Seleccionó: " + opcion);
+    }
+
+    public static TipoUsuario elegirTipo() {
+        return (TipoUsuario) JOptionPane.showInputDialog(
+                null,
+                "Seleccione tipo de usuario:",
+                "Ingreso al sistema",
+                JOptionPane.DEFAULT_OPTION,
+                null,
+                TipoUsuario.values(),
+                TipoUsuario.CLIENTE);
+    }
+
 
 }
+
+
+
