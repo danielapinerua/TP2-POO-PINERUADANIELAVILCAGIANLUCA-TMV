49f02493912269d40c2d3994812e6ed0f5186ab1
danielapinerua <daniela.pinerua@davinci.edu.ar>
2025-10-30 12:37:37 -0300

    Update Empleado.java

--- src/logicLayer/Empleado.java
+++ src/logicLayer/Empleado.java
@@ -1,5 +1,38 @@
 package logicLayer;
 
-public class Empleado {
+import javax.swing.JOptionPane;
+
+public class Empleado extends Usuario {
+	private String legajo;
+
+	public Empleado(String nombre, TipoUsuario tipo, String legajo) {
+		super(nombre, tipo);
+		this.legajo = legajo;
+	}
+
+	public String getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(String legajo) {
+		this.legajo = legajo;
+	}
+	
+
+	
+	
+	 	
+	public void ingresarLegajo() {
+	    String nombre = JOptionPane.showInputDialog("Ingrese nombre:");
+	    String legajo = JOptionPane.showInputDialog("Ingrese número de legajo:");
+
+	   
+	}
+		
+	
+
+
+	
+	
 
 }
