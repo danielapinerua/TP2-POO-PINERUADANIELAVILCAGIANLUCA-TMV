078f83958ceede394c24fcc38355d81d8f587996
danielapinerua <daniela.pinerua@davinci.edu.ar>
2025-10-30 23:21:17 -0300

    clase cajero sin metodos

--- src/logicLayer/Cajero.java
+++ src/logicLayer/Cajero.java
@@ -1,5 +1,31 @@
 package logicLayer;
 
-public class Cajero {
+import java.util.LinkedList;
 
+public class Cajero {
+	private double dineroDisponible;
+	private LinkedList<Cuenta>cuentas;
+	public Cajero(double dineroDisponible, LinkedList<Cuenta> cuentas) {
+		super();
+		this.dineroDisponible = dineroDisponible;
+		this.cuentas = cuentas;
+	}
+	public double getDineroDisponible() {
+		return dineroDisponible;
+	}
+	public void setDineroDisponible(double dineroDisponible) {
+		this.dineroDisponible = dineroDisponible;
+	}
+	public LinkedList<Cuenta> getCuentas() {
+		return cuentas;
+	}
+	public void setCuentas(LinkedList<Cuenta> cuentas) {
+		this.cuentas = cuentas;
+	}
+	@Override
+	public String toString() {
+		return "Cajero [dineroDisponible=" + dineroDisponible + ", cuentas=" + cuentas + "]";
+	}
+	
+	
 }
