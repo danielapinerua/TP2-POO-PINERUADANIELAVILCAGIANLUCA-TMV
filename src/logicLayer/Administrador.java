1719ea1ca623d3557571e3e6f5a5a80218037998
danielapinerua <daniela.pinerua@davinci.edu.ar>
2025-11-07 20:19:38 -0300

    aregue metodos a clase admin

--- src/logicLayer/Administrador.java
+++ src/logicLayer/Administrador.java
@@ -1,6 +1,50 @@
 package logicLayer;
 
+import javax.swing.JOptionPane;
+
 public class Administrador extends Usuario {
+	//dar de baja cuentas y crear empleados
+	private String idAdmin;
+
+	public Administrador(String nombre, TipoUsuario tipoUsuario, String mail, String pin, String idAdmin) {
+		super(nombre, tipoUsuario, mail, pin);
+		this.idAdmin = idAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [idAdmin=" + idAdmin + "]";
+	}
+	
+	public void crearEmpleado() {
+	    String nombre = Validar.validarLetras("Ingrese nombre del empleado:");
+	    String mail = Validar.validarCampo("Ingrese mail del empleado:");
+	    String pin = Validar.validarCampo("Ingrese PIN del empleado:");
+	    String legajo = Validar.validarCampo("Ingrese legajo del empleado:");
+
+	    Empleado nuevo = new Empleado(nombre, mail, pin, legajo);
+	    Usuario.getUsuarios().add(nuevo);
+	    JOptionPane.showMessageDialog(null, "Empleado creado con éxito.");
+	}
+	
+	public void darDeBajaCuenta() {
+	    String cbu = Validar.validarCampo("Ingrese el CBU de la cuenta a dar de baja:");
+	    Cuenta cuentaABorrar = null;
+
+	    for (Cuenta cuenta : Cuenta.getCuentas()) {
+	        if (cuenta.getCbu().equals(cbu)) {
+	            cuentaABorrar = cuenta;
+	            break;
+	        }
+	    }
+
+	    if (cuentaABorrar != null) {
+	        Cuenta.getCuentas().remove(cuentaABorrar);
+	        JOptionPane.showMessageDialog(null, "Cuenta eliminada correctamente.");
+	    } else {
+	        JOptionPane.showMessageDialog(null, "No se encontró una cuenta con ese CBU.");
+	    }
+	}
 	
 
 }
--- src/logicLayer/TipoUsuario.java
+++ src/logicLayer/TipoUsuario.java
@@ -5,7 +5,8 @@ import javax.swing.JOptionPane;
 public enum TipoUsuario {
 	
 	Cliente(new String[]{"Depositar dinero","Transferir", "Retirar dinero", "Ver saldo", "Ver movimientos","Ver informacion", "salir"}),
-	Empleado(new String[]{"Ver cuentas","Cargar dinero al cajero","Ver movimientos generales","Dar de alta cajero","Dar de baja cajero","Ver informacion", "Salir"});
+	Empleado(new String[]{"Ver cuentas","Cargar dinero al cajero","Ver movimientos generales","Dar de alta cajero","Dar de baja cajero","Ver informacion", "Salir"}),
+	Administrador(new String[]{"Crear empleado","Dar de baja cuenta","Ver informacion", "Salir"});
     //empleados puede ser dar de baja cajero
 	private String[] opciones;
 
